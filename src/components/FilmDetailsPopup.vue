baea498abb34c43334ee1a2e34b720718a6eeb49
Antoine Roumilhac <antoine.roumilhac@outlook.fr>
2022-01-10 19:55:43 +0100

    Added a button to copy film json data

--- src/components/FilmDetailsPopup.vue
+++ src/components/FilmDetailsPopup.vue
@@ -6,15 +6,26 @@
           <h5 class="modal-title h-100 fw-bolder">{{ movieData.title }}
             <span class="badge rounded-pill bg-secondary align-baseline">{{ movieData.release_date }}</span></h5>
         </div>
-        <div class="modal-body" @scroll="scrolled = $event.target.scrollTop > 0">
+        <div class="modal-body" id="popupBody" @scroll="scrolled = $event.target.scrollTop > 0">
           <p class="fs-6 text-start w-75">{{ movieData.original_title }}</p>
-          <StarRating class="position-absolute top-0 end-0 m-3" :rating="parseInt(movieData.rt_score)" :max_stars="5"></StarRating>
-          <img alt="Film image" v-bind:src="movieData.image" class="w-100 rounded"/>
+          <StarRating class="position-absolute top-0 end-0 m-3" :rating="parseInt(movieData.rt_score)"
+                      :max_stars="5"></StarRating>
+          <img alt="Film image" :src="movieData.poster" class="w-100 rounded" @dragstart.prevent/>
           <p class="fs-4 mt-3 fw-bold text-start">{{ movieData.director }}</p>
           <p class="fs-6 m-4">{{ movieData.description }}</p>
+          <div class="alert alert-success" role="alert" v-if="showCopySuccess">
+            Data has been copied to clipboard.
+          </div>
+          <div class="d-flex align-content-start">
+            <button type="button" class="btn btn-outline-secondary btn-sm" id="copyButton" @click="dataToClip" data-toggle="tooltip" data-bs-placement="right" title="Copy data to clipboard">
+              <i class="bi bi-clipboard"></i>
+            </button>
+          </div>
         </div>
         <div class="modal-footer justify-content-center">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" @click="scrolled = false">Close</button>
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" @click="scrolled = false">
+            Close
+          </button>
         </div>
       </div>
     </div>
@@ -23,17 +34,36 @@
 
 <script>
 import StarRating from "@/components/StarRating";
+import {Tooltip} from 'bootstrap';
 
 export default {
   name: "FilmDetailsPopup",
   components: {StarRating},
   data() {
     return {
-      scrolled: false
+      scrolled: false,
+      showCopySuccess: false,
+      copyButtonTooltip: Tooltip,
+      copyTimeout: null
     }
   },
   props: {
     movieData: Object
+  },
+  mounted() {
+    this.copyButtonTooltip = new Tooltip(document.getElementById('copyButton'))
+  },
+  updated() {
+    this.copyButtonTooltip = new Tooltip(document.getElementById('copyButton'))
+  },
+  methods: {
+    dataToClip() {
+      clearTimeout(this.copyTimeout)
+      this.copyButtonTooltip.hide()
+      navigator.clipboard.writeText(JSON.stringify(this.movieData, null, "\t"))
+      this.showCopySuccess = true
+      this.copyTimeout = setTimeout(() => this.showCopySuccess = false, 5000)
+    }
   },
   computed: {
     toHours(inMinutes) {
@@ -46,6 +76,4 @@ export default {
 </script>
 
 <style scoped>
-.modal-title * {
-}
 </style>
